04e5c3ff2656ebd6f5704967302c56d57912c992
Krys Petrie <krys.petrie@trinetx.com>
2018-04-18 17:52:15 -0400

    Integration complete.

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-var fs = require('fs')
 var GitHubApi = require('@octokit/rest');
+var dateFormat = require('dateformat');
 var github = new GitHubApi({
     version: '3.0.0'
 });
 
 exports.handler = function(event, context) {
-    var githubEvent = event.Records[0].Sns.Message;
+    const githubEvent = event.Records[0].Sns.Message;   
     console.log('Received GitHub event:', githubEvent);
-
-    if (!githubEvent.hasOwnProperty('issue') || githubEvent.action !== 'opened') {
-        // Not an event for opening an issue
-        context.succeed();
-    }
+    
     // Authenticate to comment on the issue
     github.authenticate({
         type: 'oauth',
         token: process.env.GITHUBTOKEN
     });
 
-    var poster = githubEvent.issue.user.login;
+    const body = JSON.parse(githubEvent);
+    
+    const createIssue = {
+        owner: body.repository.owner.login,
+        repo: body.repository.name,
+        title: "Lambda Hook! " + dateFormat(Date.now(), "dddd, mmmm dS, yyyy, h:MM:ss TT"),
+        body: "Hi @" + body.repository.owner.name + "!\n\n" +
+              "Something triggered me to open another issue! Sorry!"
+    };
 
-    github.issues.createComment({
-        user: githubEvent.repository.owner.login,
-        repo: githubEvent.repository.name,
-        number: githubEvent.issue.number,
-        body: "Hi @" + poster + "!\n" +
-              "\n" +
-              "Thank you for your interest in this project! Unfortunately, we're " +
-              "really busy at the moment, but we'll get to your issue as soon as " +
-              "possible. Have a great day!"
-    }, context.done);
+    console.log('Creating issue:', JSON.stringify(createIssue));
+    github.issues.create(createIssue, context.done);
 };
